e3ce2509a353e79ea3a8deb6f0cde5519b708610
Dhananjay Gupta <82753264+dhan2501@users.noreply.github.com>
2025-04-08 13:43:54 +0530

    price remove from model

--- core/models.py
+++ core/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='products/')
     description = models.TextField(blank=True, null=True)
 
@@ -15,3 +14,4 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
