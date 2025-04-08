e9d4ed916fa788d3825120e126f2d49984619359
Dhananjay Gupta <82753264+dhan2501@users.noreply.github.com>
2025-04-08 13:56:33 +0530

    create menu, logo models

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Logo, MenuItem, SocialMediaLink,
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
+
+
+@admin.register(Logo)
+class LogoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'image', 'alt_text')
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url', 'parent', 'position','order', 'is_active')
+    list_filter = ('parent', 'position', 'is_active')
+    ordering = ('order',)
+
+
+@admin.register(SocialMediaLink)
+class SocialMediaLinkAdmin(admin.ModelAdmin):
+    list_display = ('platform', 'url', 'location', 'is_active')
+    list_filter = ('location', 'is_active')
+    search_fields = ('platform', 'url')
