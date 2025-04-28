591b909e3db962406852d249d272f4037c2b251e
Dhananjay Gupta <82753264+dhan2501@users.noreply.github.com>
2025-04-28 14:10:12 +0530

    Deleted All Data

--- core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from core.views import index
-
-urlpatterns = [
-    path('api/products/', views.product_list, name='product-list'),
-    path('', index),
-]
